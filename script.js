cd07096894c338a8164c7c7188881997d7c7b4a3
Jim <jim.haz34@gmail.com>
2021-12-05 14:04:55 -0500

    refactoring code to be more readable

--- script.js
+++ script.js
@@ -3,25 +3,22 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
+// console.log(btnsOpenModal);
 
 const openModal = function() {
-  console.log('button clicked');
+  // console.log('button clicked');
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
+}
+const closeModal = function() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
 }
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener('click', openModal);
-
-  const closeModal = function() {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-  }
-
-  btnCloseModal.addEventListener('click', closeModal)
-  overlay.addEventListener('click', closeModal)
-
 }
+
+btnCloseModal.addEventListener('click', closeModal)
+overlay.addEventListener('click', closeModal)
